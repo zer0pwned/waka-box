5ecbe567d05a5b8a5eff9875740e9a775c1380b1
NULL <3125626+zer0pwned@users.noreply.github.com>
2020-12-25 17:58:00 -0500

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -872,7 +872,7 @@ module.exports = (function(e, t) {
           gist_id: o,
           files: {
             [e]: {
-              filename: `📊 Weekly development breakdown`,
+              filename: ` ✨Weekly development breakdown✨`,
               content: r.join("\n")
             }
           }
